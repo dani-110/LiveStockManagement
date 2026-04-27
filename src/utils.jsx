23cd33542c0478e261a6413cd6c4025172dda372
daniyal.hussain <daniyal.hussain@bssuniversal.com>
2026-04-27 21:11:05 +0500

    update user list

--- src/utils.jsx
+++ src/utils.jsx
@@ -65,55 +65,55 @@ export const expenseType = [
     },
 ]
 
-export const users = [
-    {
-        value: 1,
-        name: "user1"
-    },
-    {
-        value: 2,
-        name: "admin"
-    },
-    {
-        value: 3,
-        name: "user3"
-    },
-    {
-        value: 4,
-        name: "user4"
-    },
-    {
-        value: 5,
-        name: "user2"
-    },
-]
 // export const users = [
 //     {
-//         value: 6,
+//         value: 1,
 //         name: "user1"
 //     },
 //     {
-//         value: 7,
-//         name: "user2"
+//         value: 2,
+//         name: "admin"
 //     },
 //     {
-//         value: 8,
+//         value: 3,
 //         name: "user3"
 //     },
 //     {
-//         value: 9,
+//         value: 4,
 //         name: "user4"
 //     },
 //     {
-//         value: 10,
-//         name: "admin1"
-//     },
-//     {
-//         value: 11,
-//         name: "admin2"
-//     },
-//     {
-//         value: 12,
-//         name: "admin3"
+//         value: 5,
+//         name: "user2"
 //     },
 // ]
+export const users = [
+    {
+        value: 6,
+        name: "user1"
+    },
+    {
+        value: 7,
+        name: "user2"
+    },
+    {
+        value: 8,
+        name: "user3"
+    },
+    {
+        value: 9,
+        name: "user4"
+    },
+    {
+        value: 10,
+        name: "admin1"
+    },
+    {
+        value: 11,
+        name: "admin2"
+    },
+    {
+        value: 12,
+        name: "admin3"
+    },
+]
